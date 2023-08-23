a8d01a5a75a2c8eea2af87af2340ed3624458f20
itsmehrd <kingharish22@gmail.com>
2023-08-23 05:56:47 +0530

    11. HOME and PAGE_NOT_FOUND routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
